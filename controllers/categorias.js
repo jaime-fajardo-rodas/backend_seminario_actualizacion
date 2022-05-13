ad600e0f472d2e5395185f35516cc8f9aede0895
Jdizzett <jdizzett@gmail.com>
2022-05-12 21:19:38 -0500

    controlador y modelo categoria
    se crea controlador y modelo de categorias

---
+++ controllers/categorias.js
@@ -0,0 +1,66 @@
+const { response, request } = require("express");
+const Usuario = require("../models/categoria");
+const bcryptjs = require("bcryptjs");
+
+const categoriasGet = async (req = request, res = response) => {
+  
+  const { desde = 0, limite = 5 } = req.query;
+  const query = { estado: true };
+
+  const [total, categorias] = await Promise.all([
+    Categoria.countDocuments(query),
+    Categoria.find(query).skip(Number(desde)).limit(Number(limite)),
+  ]);
+
+  res.json({
+    total,
+    categorias,
+  });
+};
+
+const categoriasPut = async (req, res) => {
+  const { id } = req.params;
+  const { _id, nombres, tipo_categoria, ...resto } = req.body;
+
+
+
+  await Categoria.findByIdAndUpdate(id, resto);
+  const categoria = await Categoria.findById(id);
+
+  res.json({categoria});
+};
+
+const categoriasPost = async (req, res) => {
+  const { nombres, tipo_categoria } = req.body;
+  const categoria = new Categoria({ nombres, tipo_categoria });
+
+
+  //Guardar en BD
+  await categoria.save();
+
+  res.json({
+    categoria,
+  });
+};
+
+const categoriasDelete = async(req, res) => {
+  const { id } = req.params;
+  const { borrar_permanente } = req.body;
+
+  await Categoria.findByIdAndUpdate( id, {estado:false} );
+  const categoria = await Categoria.findById(id);
+
+  if(borrar_permanente === true){
+    await Categoria.findByIdAndDelete(id);
+  }
+
+  res.json({categoria});
+
+}
+
+module.exports = {
+  categoriasGet,
+  categoriasPut,
+  categoriasPost,
+  categoriasDelete,
+};
---
+++ models/categoria.js
@@ -0,0 +1,27 @@
+
+const { Schema, model } = require('mongoose');
+
+const Categoriaschema = Schema({
+    nombres: {
+        type: String,
+        required: [true, 'nombres es obligatorio'],
+    },
+    tipo_categoria: {
+        type: String,
+        required: [true, 'Tipo categoria es obligatorio'],
+    },
+   
+    estado: {
+        type: Boolean,
+        default: true
+    },
+
+});
+
+CategoriaSchema.methods.toJSON = function(){
+    const {__v,nombres, tipo_categoria} = this.toObject();
+    categoria.uid = _id;
+    return categoria;
+}
+
+module.exports = model( 'Categoria', CategoriaSchema );
